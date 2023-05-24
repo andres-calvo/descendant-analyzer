4a709e360d8ca15f332ba7e2f03da95b08c3824e
Andres Calvo <calvo.andres.tech@gmail.com>
2023-05-23 22:51:35 -0500

    Fix infinite Loops

--- src/components/animated-pila.tsx
+++ src/components/animated-pila.tsx
@@ -1,59 +1,54 @@
-import { animated, useTransition, Controller } from "@react-spring/web";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { animated, useTransition } from "@react-spring/web";
+import { useEffect, useRef, useState } from "react";
 import { ArraySubscription } from "../utils/array-subscription";
 import { analizeString } from "../utils/main";
-let timeoutsArr: Array<number> = [];
 export const AnimatedPila = () => {
   const [pila, setPila] = useState(["$", "E"]);
   const [stringToAnalize, setStringToAnalize] = useState("");
-  
+
   const transitions = useTransition(pila, {
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0, color: "red" },
   });
- const [isAnimating, setIsAnimating] = useState(false)
+  const [isAnimating, setIsAnimating] = useState(false);
   const currentTimeline = useRef(300);
   const inputRef = useRef<HTMLInputElement>(null);
-  const arraySubscription = useMemo(
-    () =>
-      new ArraySubscription({
-        onPush: (newItems) => {
-          currentTimeline.current = currentTimeline.current + 1000;
-          newItems.forEach((item, i) => {
-            currentTimeline.current = currentTimeline.current + 300;
 
-            timeoutsArr.push(
-              setTimeout(() => {
-                setPila((prevPila) => prevPila.concat(item));
-              }, currentTimeline.current)
-            );
-          });
-        },
-        onPop: () => {
-          currentTimeline.current = currentTimeline.current + 500;
+  useEffect(() => {
+    if (stringToAnalize.length == 0) return;
+    const timeoutsArr: Array<number> = [];
+    const arraySubscription = new ArraySubscription({
+      onPush: (newItems) => {
+        currentTimeline.current = currentTimeline.current + 1000;
+        newItems.forEach((item, i) => {
+          currentTimeline.current = currentTimeline.current + 300;
+
           timeoutsArr.push(
             setTimeout(() => {
-              setPila((prevPila) => {
-                //Si es el ultimo elemento a remover, habilita el boton de analizar
-                if(prevPila.length == 1){
-                  setIsAnimating(false)
-                }
-                return prevPila.slice(0, -1)
-              } );
-             
+              setPila((prevPila) => prevPila.concat(item));
             }, currentTimeline.current)
           );
-        },
-      }),
-    []
-  );
-  useEffect(() => {
-    if(stringToAnalize.length > 0) {
-      analizeString(stringToAnalize, arraySubscription);
-      setIsAnimating(true)
-    }
-
+        });
+      },
+      onPop: () => {
+        currentTimeline.current = currentTimeline.current + 500;
+        timeoutsArr.push(
+          setTimeout(() => {
+            setPila((prevPila) => {
+              //Si es el ultimo elemento a remover, habilita el boton de analizar
+              if (prevPila.length == 1) {
+                setIsAnimating(false);
+                currentTimeline.current = 300;
+              }
+              return prevPila.slice(0, -1);
+            });
+          }, currentTimeline.current)
+        );
+      },
+    });
+    analizeString(stringToAnalize, arraySubscription);
+    setIsAnimating(true);
     return () => {
       timeoutsArr.forEach((timeout) => {
         clearTimeout(timeout);
@@ -61,18 +56,12 @@ export const AnimatedPila = () => {
     };
   }, [stringToAnalize]);
 
-
-  
-
   return (
     <div className="flex flex-col items-center w-full max-w-2xl  mt-10 ">
       <form
         className="flex w-full  gap-2 "
         onSubmit={(e) => {
           e.preventDefault();
-          timeoutsArr.forEach((timeout) => {
-            clearTimeout(timeout);
-          });
           const input = inputRef.current;
           const newString = input?.value ?? "";
           if (newString.length == 0) return;
--- src/utils/main.ts
+++ src/utils/main.ts
@@ -2,14 +2,18 @@ import type { ArraySubscription } from "./array-subscription";
 import { Pila } from "./pila";
 import { tablaLL1, TokensType, NoTerminalesType } from "./utils";
 
-export function analizeString(inputString:string,arraySubscription:ArraySubscription){
+export function analizeString(
+  inputString: string,
+  arraySubscription: ArraySubscription
+) {
   const tokens = inputString.split(" ") as Array<TokensType>;
   const myPila = new Pila(arraySubscription);
   myPila.log();
   console.log(inputString);
+  console.log(tokens);
   for (const token of tokens) {
     let shouldLoop = true;
-    while (shouldLoop || (shouldLoop && myPila.length > 0)) {
+    while (shouldLoop) {
       let lastElement = myPila.getTopItem();
       const substitution = tablaLL1[token][
         lastElement as NoTerminalesType
@@ -20,11 +24,14 @@ export function analizeString(inputString:string,arraySubscription:ArraySubscrip
       }
       myPila.log();
       lastElement = myPila.getTopItem() as TokensType;
-  
       if (tokens.includes(lastElement)) {
         myPila.pop();
         inputString = inputString.replace(token, "");
         //Go to next token
+        shouldLoop = false;
+      }
+      //This prevents infinite loops
+      if (myPila.length == 0) {
         shouldLoop = false;
       }
     }
